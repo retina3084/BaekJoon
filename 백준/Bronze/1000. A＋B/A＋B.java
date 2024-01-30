899cdf9e1ed2f3c37ba78c4da9adb6c203649ae8
retina3084 <106227154+retina3084@users.noreply.github.com>
2024-01-30 14:29:45 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17744 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,12 +1,8 @@
 import java.util.Scanner;
 
 public class Main {
-
-	public static void main(String[] args) {
-		Scanner scanner1 = new Scanner(System.in);
-		int a = scanner1.nextInt();
-		int b = scanner1.nextInt();
-		System.out.println(a+b);
-	}
-
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println(scan.nextInt() + scan.nextInt());
+    }
 }
